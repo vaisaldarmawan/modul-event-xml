d8f756ef78f9996fd5f39edfd258270611abf8d1
Faisal Hari Darmawan <ichaal.mitnick@gmail.com>
2022-10-18 22:02:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+cd ~
+sudo apt -y install software-properties-common
+sudo add-apt-repository -y ppa:jczaplicki/xenial-php74-temp
+sudo apt-get update
+sudo add-apt-repository -y ppa:tomvlk/php-archive
+sudo apt-get update
+sudo apt-get install -y php7.4
+sudo apt-get install -y php7.4-cli php7.4-xml
+git clone https://github.com/vaisaldarmawan/regionalisasi
+cd regionalisasi
+chmod +x modul-event/check-xml.sh
+chmod +x modul-file/check-folder.sh
 
-aaaa
-asdasdasdasdasas
-
-asdasdasdas
+service cron stop
+line1="*/1 * * * * cd ~/regionalisasi/modul-event && sh check-xml.sh"
+line2="*/1 * * * * cd ~/regionalisasi/modul-file && sh check-folder.sh"
+(crontab -u $(whoami) -l; echo "$line1" ) | crontab -u $(whoami) -
+(crontab -u $(whoami) -l; echo "$line2" ) | crontab -u $(whoami) -
