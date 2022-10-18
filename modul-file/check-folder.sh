c8db13d2f853f6f3e83d130fc3ef1e460b291b25
Faisal Hari Darmawan <ichaal.mitnick@gmail.com>
2022-10-18 22:04:21 +0700

    Update check-folder.sh

--- modul-file/check-folder.sh
+++ modul-file/check-folder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/sysop/regionalisasi/config.cfg
+source ~/regionalisasi/config.cfg
 
 rsync -avuzr --out-format="%t %f" --timeout=60 --include="*/" --include=""$folder_jpg/$nama_jpg"" --include=""$folder_sensor/$nama_sensor"" --exclude="*"  -e "ssh -p $target_port_ssh" $dir_sumber_data $target_user_ssh@$target_ip_ssh:$dir_target_data/$nama_upt
