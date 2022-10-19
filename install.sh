7994a97e60a032b62c64d0c26d684f41a2da1a77
Faisal Hari Darmawan <ichaal.mitnick@gmail.com>
2022-10-19 16:58:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,17 +17,21 @@ echo " >> Update Repository & Package (harap menunggu)"
 sudo apt-get update > /dev/null 2>&1
 
 echo " >> Menginstall php7.4, php7.4-cli, php7.4-xml, php7.4-mysql"
+sudo sleep 1s
 sudo apt-get install -y php7.4 > /dev/null 2>&1
 sudo apt-get install -y php7.4-cli php7.4-xml php7.4-mysql > /dev/null 2>&1
 
 echo " >> Menginstall git"
+sudo sleep 1s
 sudo apt-get install -y git > /dev/null 2>&1
 
 echo " >> Menghentikan Service Crontab"
+sudo sleep 1s
 sudo service cron stop > /dev/null 2>&1
 
 cd
 echo " >> Mengunduh & Menginstall File Regionalisasi"
+sudo sleep 1s
 git clone https://github.com/vaisaldarmawan/regionalisasi > /dev/null 2>&1
 rm -rf regionalisasi/.git
 
@@ -36,6 +40,7 @@ chmod +x modul-event/check-xml.sh
 chmod +x modul-file/check-folder.sh
 
 echo " >> Memasukan command kedalam crontab"
+sudo sleep 1s
 line1="*/1 * * * * cd ~/regionalisasi/modul-event && sh check-xml.sh"
 line2="*/1 * * * * cd ~/regionalisasi/modul-file && sh check-folder.sh"
 (crontab -u $(whoami) -l; echo "$line1" ) | crontab -u $(whoami) -
@@ -44,12 +49,14 @@ line2="*/1 * * * * cd ~/regionalisasi/modul-file && sh check-folder.sh"
 cd
 
 echo " >> Mengunduh File Monitoring"
+sudo sleep 1s
 git clone https://github.com/sukmacakrawala/monitoring-seiscomp3 > /dev/null 2>&1
 rm -rf monitoring-seiscomp3/.git
 mv monitoring-seiscomp3 remote_site
 chmod +x remote_site/seischeck.sh
 
 echo " >> Memasukan command kedalam crontab"
+sudo sleep 1s
 line3="*/1 * * * * ~/remote_site/seischek.sh"
 (crontab -u $(whoami) -l; echo "$line3" ) | crontab -u $(whoami) -
 
