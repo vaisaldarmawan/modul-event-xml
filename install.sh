6d88b6e4169bcc63e265e7e975529d1721727325
Faisal Hari Darmawan <ichaal.mitnick@gmail.com>
2022-10-19 15:44:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 cd
-#sudo apt install -y software-properties-common
-#sudo add-apt-repository -y ppa:jczaplicki/xenial-php74-temp
-#sudo apt-get update
-#sudo add-apt-repository -y ppa:tomvlk/php-archive
-#sudo apt-get update
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:jczaplicki/xenial-php74-temp
+sudo apt-get update
+sudo add-apt-repository -y ppa:tomvlk/php-archive
+sudo apt-get update
 sudo apt-get install -y php7.4
 sudo apt-get install -y php7.4-cli php7.4-xml php7.4-mysql
 sudo apt-get install -y git
+sudo service cron stop;
 
+cd
 git clone https://github.com/vaisaldarmawan/regionalisasi
+rm -rf regionalisasi/.git
+
 cd regionalisasi
 chmod +x modul-event/check-xml.sh
 chmod +x modul-file/check-folder.sh
 
 
-service cron stop
 line1="*/1 * * * * cd ~/regionalisasi/modul-event && sh check-xml.sh"
 line2="*/1 * * * * cd ~/regionalisasi/modul-file && sh check-folder.sh"
 (crontab -u $(whoami) -l; echo "$line1" ) | crontab -u $(whoami) -
 (crontab -u $(whoami) -l; echo "$line2" ) | crontab -u $(whoami) -
 
-
+cd
 git clone https://github.com/sukmacakrawala/monitoring-seiscomp3
-mv -r monitoring-seiscomp3 remote_site
+rm -rf monitoring-seiscomp3/.git
+mv monitoring-seiscomp3 remote_site
 chmod +x remote_site/seischeck.sh
 
-line3="1 * * * * ~/remote_site/seischek.sh"
+line3="*/1 * * * * ~/remote_site/seischek.sh"
 (crontab -u $(whoami) -l; echo "$line3" ) | crontab -u $(whoami) -
 
 echo "Installasi Selesai !!"
